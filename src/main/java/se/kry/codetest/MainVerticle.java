daa556b8ec912b6141143b8fec6da9edb9830428
Mohammad Fathizadeh <mohammad.fathizadeh@gmail.com>
2020-04-08 13:24:18 +0200

    implementing getService endpoint to fetch service list

--- src/main/java/se/kry/codetest/MainVerticle.java
+++ src/main/java/se/kry/codetest/MainVerticle.java
@@ -8,62 +8,75 @@ import io.vertx.ext.web.Router;
 import io.vertx.ext.web.handler.BodyHandler;
 import io.vertx.ext.web.handler.StaticHandler;
 
-import java.util.HashMap;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class MainVerticle extends AbstractVerticle {
 
-  private HashMap<String, String> services = new HashMap<>();
-  //TODO use this
-  private DBConnector connector;
-  private BackgroundPoller poller = new BackgroundPoller();
+    private DBConnector connector;
+    private BackgroundPoller poller = new BackgroundPoller();
 
-  @Override
-  public void start(Future<Void> startFuture) {
-    connector = new DBConnector(vertx);
-    Router router = Router.router(vertx);
-    router.route().handler(BodyHandler.create());
-    services.put("https://www.kry.se", "UNKNOWN");
-    vertx.setPeriodic(1000 * 60, timerId -> poller.pollServices(services));
-    setRoutes(router);
-    vertx
-        .createHttpServer()
-        .requestHandler(router)
-        .listen(8080, result -> {
-          if (result.succeeded()) {
-            System.out.println("KRY code test service started");
-            startFuture.complete();
-          } else {
-            startFuture.fail(result.cause());
-          }
+    @Override
+    public void start(Future<Void> startFuture) {
+        connector = new DBConnector(vertx);
+        Router router = Router.router(vertx);
+        router.route().handler(BodyHandler.create());
+        vertx.setPeriodic(1000 * 60, timerId -> poller.pollServices());
+        setRoutes(router);
+        vertx
+                .createHttpServer()
+                .requestHandler(router)
+                .listen(8080, result -> {
+                    if (result.succeeded()) {
+                        System.out.println("KRY code test service started");
+                        startFuture.complete();
+                    } else {
+                        startFuture.fail(result.cause());
+                    }
+                });
+    }
+
+    private void setRoutes(Router router) {
+        router.route("/*").handler(StaticHandler.create());
+
+        router.get("/service").handler(req -> {
+
+            connector.query("SELECT id,url,description, status,creation_time, last_poll_time FROM service").setHandler(event -> {
+                List<JsonObject> jsonServices = event.result().getRows()
+                        .stream()
+                        .map(this::convertRowToJsonObject)
+                        .collect(Collectors.toList());
+
+                req.response()
+                        .putHeader("content-type", "application/json")
+                        .end(new JsonArray(jsonServices).encode());
+
+
+            });
+
         });
-  }
 
-  private void setRoutes(Router router){
-    router.route("/*").handler(StaticHandler.create());
-    router.get("/service").handler(req -> {
-      List<JsonObject> jsonServices = services
-          .entrySet()
-          .stream()
-          .map(service ->
-              new JsonObject()
-                  .put("name", service.getKey())
-                  .put("status", service.getValue()))
-          .collect(Collectors.toList());
-      req.response()
-          .putHeader("content-type", "application/json")
-          .end(new JsonArray(jsonServices).encode());
-    });
-    router.post("/service").handler(req -> {
-      JsonObject jsonBody = req.getBodyAsJson();
-      services.put(jsonBody.getString("url"), "UNKNOWN");
-      req.response()
-          .putHeader("content-type", "text/plain")
-          .end("OK");
-    });
-  }
 
+    }
+
+    private JsonObject convertRowToJsonObject(JsonObject row) {
+        return new JsonObject()
+                .put("id", row.getInteger("id"))
+                .put("url", row.getString("url"))
+                .put("status", row.getString("status"))
+                .put("creationTime", dateTimeToString(row.getLong("creation_time")))
+                .put("lastPollTime", dateTimeToString(row.getLong("last_poll_time")))
+                .put("description", row.getString("description"));
+    }
+
+    private String dateTimeToString(Long dateTime) {
+        if (dateTime == null)
+            return null;
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        return sdf.format(new Timestamp(dateTime));
+    }
 }
 
 
