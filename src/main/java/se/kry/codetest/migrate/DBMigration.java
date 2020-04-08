66a93ba1f9827991a1f40ccc7170ca0201a38823
Mohammad Fathizadeh <mohammad.fathizadeh@gmail.com>
2020-04-08 13:01:54 +0200

    changed migration -  created new service table with new columns

--- src/main/java/se/kry/codetest/migrate/DBMigration.java
+++ src/main/java/se/kry/codetest/migrate/DBMigration.java
@@ -8,8 +8,20 @@ public class DBMigration {
   public static void main(String[] args) {
     Vertx vertx = Vertx.vertx();
     DBConnector connector = new DBConnector(vertx);
-    connector.query("CREATE TABLE IF NOT EXISTS service (url VARCHAR(128) NOT NULL)").setHandler(done -> {
+    connector.query("create table service\n" +
+            "(\n" +
+            "\turl VARCHAR(128) not null,\n" +
+            "\tdescription VARCHAR(128),\n" +
+            "\tstatus VARCHAR(10),\n" +
+            "\tid INTEGER\n" +
+            "\t\tconstraint service_pk\n" +
+            "\t\t\tprimary key autoincrement,\n" +
+            "\tcreation_time INTEGER,\n" +
+            "\tlast_poll_time integer\n" +
+            ");\n" +
+            "\n").setHandler(done -> {
       if(done.succeeded()){
+
         System.out.println("completed db migrations");
       } else {
         done.cause().printStackTrace();
