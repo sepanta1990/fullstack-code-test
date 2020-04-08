b255384558c3cc575fedd1bcd5989ecca7519631
Mohammad Fathizadeh <mohammad.fathizadeh@gmail.com>
2020-04-08 13:26:07 +0200

    implementing getService test to test getService endpoint

--- src/test/java/se/kry/codetest/TestMainVerticle.java
+++ src/test/java/se/kry/codetest/TestMainVerticle.java
@@ -1,7 +1,7 @@
 package se.kry.codetest;
 
 import io.vertx.core.Vertx;
-import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.client.WebClient;
 import io.vertx.junit5.Timeout;
 import io.vertx.junit5.VertxExtension;
@@ -11,6 +11,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -18,23 +20,22 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @ExtendWith(VertxExtension.class)
 public class TestMainVerticle {
 
-  @BeforeEach
-  void deploy_verticle(Vertx vertx, VertxTestContext testContext) {
-    vertx.deployVerticle(new MainVerticle(), testContext.succeeding(id -> testContext.completeNow()));
-  }
 
-  @Test
-  @DisplayName("Start a web server on localhost responding to path /service on port 8080")
-  @Timeout(value = 10, timeUnit = TimeUnit.SECONDS)
-  void start_http_server(Vertx vertx, VertxTestContext testContext) {
-    WebClient.create(vertx)
-        .get(8080, "::1", "/service")
-        .send(response -> testContext.verify(() -> {
-          assertEquals(200, response.result().statusCode());
-          JsonArray body = response.result().bodyAsJsonArray();
-          assertEquals(1, body.size());
-          testContext.completeNow();
-        }));
-  }
+    @BeforeEach
+    void deploy_verticle(Vertx vertx, VertxTestContext testContext) {
+        vertx.deployVerticle(new MainVerticle(), testContext.succeeding(id -> testContext.completeNow()));
+    }
+
+    @Test
+    @DisplayName("Start a web server on localhost responding to path /service on port 8080")
+    @Timeout(value = 10, timeUnit = TimeUnit.SECONDS)
+    void start_http_server(Vertx vertx, VertxTestContext testContext) {
+        WebClient.create(vertx)
+                .get(8080, "::1", "/service")
+                .send(response -> testContext.verify(() -> {
+                    assertEquals(200, response.result().statusCode());
+                    testContext.completeNow();
+                }));
+    }
 
 }
